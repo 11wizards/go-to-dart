3a5baf439cba35c7a6473b60e384e624d8de1d20
Diego Frata <diego.frata@11wizards.com>
2024-11-22 19:09:01 +0100

    fix: should use utc dates back and forth

--- generator/dart/timestamp_converter.dart
+++ generator/dart/timestamp_converter.dart
@@ -2,8 +2,8 @@ class _TimestampConverter implements JsonConverter<DateTime, Timestamp> {
   const _TimestampConverter();
 
   @override
-  DateTime fromJson(Timestamp json) => json.toDate();
+  DateTime fromJson(Timestamp json) => DateTime.fromMicrosecondsSinceEpoch(json.microsecondsSinceEpoch, isUtc: true);
 
   @override
-  Timestamp toJson(DateTime object) => Timestamp.fromDate(object);
+  Timestamp toJson(DateTime object) => Timestamp.fromDate(object.toUtc());
 }
